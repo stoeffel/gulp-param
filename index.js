a682c406b006b3223737b485ae7df18c5111ec4d
przemcio <przemyslaw.olech@gmail.com>
2016-01-28 21:23:00 +0100

    refactoring step 2, add support for callback method, update test to be more BDD

--- index.js
+++ index.js
@@ -1,49 +1,39 @@
-var retrieveArguments = require('retrieve-arguments'),
-    includes = require('lodash.includes'), minimist = require('minimist'),
-  stringify = require('node-stringify');
+var retrieveArguments = require('retrieve-arguments'), minimist = require('minimist'),
+  stringify = require('node-stringify'), debug = require('debug')('gulp-param');
 
-module.exports = function(gulp, processArgv) {
+module.exports = function (gulp, processArgv, callbackFunctionName) {
+  var parsedCmdArguments = minimist(processArgv);
+  var prepareArgumentsArray = function (functionArguments, originalCallbackFunction) {
+    var arguments = [];
+    debug('prepare argument base on function arguments (%s) and parsed commandline (%s)',
+      functionArguments, stringify(parsedCmdArguments));
 
-
-  var  getInjections = function(functionArguments, parsedCmdArguments, originalCallbackFunction) {
-    var injections = [], callbackinjected = false;
-
-    console.log('getInjections ('+ functionArguments+')('+stringify(parsedCmdArguments)+')');
     for (var i = 0; i < functionArguments.length; i++) {
       var functionArgument = functionArguments[i], value = parsedCmdArguments[functionArgument];
-      if(value) {
-        injections.push(value);
+      if (value) {
+        arguments.push(value);
+      } else if (functionArgument === callbackFunctionName) {
+        arguments.push(originalCallbackFunction);
       }
     }
-    return injections;
+    return arguments;
   };
-  var wrappedTask = function(taskName, taskDependencies, taskDefinition) {
-    console.log('run task ');
-    var functionArguments, cmdLineArguments, wrappedFunction;
 
+  var wrappedTask = function (taskName, taskDependencies, taskDefinition) {
     if (!taskDefinition && typeof taskDependencies === 'function') {
       taskDefinition = taskDependencies;
       taskDependencies = undefined;
     }
+    taskDefinition = taskDefinition || function () {};
 
-    taskDependencies = taskDependencies || [];
-
-    taskDefinition = taskDefinition || function() {};
-    functionArguments = retrieveArguments(taskDefinition);
-
-    console.log("functionArguments ("+functionArguments+") form "+taskDefinition);
-
-    cmdLineArguments = minimist(processArgv);
-
-    wrappedFunction = function(originalCallbackFunction) {
-      return taskDefinition.apply(gulp, getInjections(functionArguments, cmdLineArguments, originalCallbackFunction));
+    var wrappedFunction = function (originalCallbackFunction) {
+      return taskDefinition.apply(gulp, prepareArgumentsArray(retrieveArguments(taskDefinition), originalCallbackFunction));
     };
-
-    return gulp.task.call(gulp, taskName, taskDependencies, wrappedFunction);
+    return gulp.task.call(gulp, taskName, taskDependencies || [], wrappedFunction);
   };
 
-  var wrappedGulp = {task:wrappedTask};  //should be better
+  var wrappedGulp = {task: wrappedTask};  //should be better
   wrappedGulp.prototype = gulp;
-
+  wrappedGulp.constructor = gulp.constructor;
   return wrappedGulp;
 };
--- test/gulp_param_spec.js
+++ test/gulp_param_spec.js
@@ -1,5 +1,5 @@
 var mockedGulp = {
-    task: function(name, dep, fn) {
+    task: function (name, dep, fn) {
       console.log('mockedGulp run task ');
       if (!dep) {
         fn.call({}, name, fn);
@@ -9,58 +9,69 @@ var mockedGulp = {
     }
   },
   gulpParam = require('./../index.js'),
-  assert = require('assert');
+  expect = require('chai').expect;
 
-describe('gulp-param', function() {
+describe('gulp-param', function () {
 
-  describe('#task', function() {
+  describe('#task', function () {
 
-    it('should run the task', function(done) {
+    it('should run the task', function (done) {
 
-      var gulp =  gulpParam(mockedGulp, ['', '', '']);
-      gulp.task('test', function() {
+      var gulp = gulpParam(mockedGulp, ['', '', '']);
+      gulp.task('test', function () {
         done();
       });
     });
 
-    it('should pass "moo" in the "param"-argument', function(done) {
+    it('should pass "moo" in the "param"-argument', function (done) {
       console.log("init gulp");
-      var gulp =  gulpParam(mockedGulp, ['', '', '', '--param', 'moo']);
+      var gulp = gulpParam(mockedGulp, ['', '', '', '--param', 'moo']);
       console.log("finished gulp");
-      gulp.task('test', function(param) {
-        assert.equal(param, 'moo');
+      gulp.task('test', function (param) {
+        expect(param).to.equal('moo');
         done();
       });
     });
 
-    it('should pass "null" in the "param"-argument if nothing is passed', function(done) {
-      var gulp =  gulpParam(mockedGulp,  ['', '', '']);
-      gulp.task('test', function(param) {
-        assert.equal(param, null);
+    it('should pass "null" in the "param"-argument if nothing is passed', function (done) {
+      var gulp = gulpParam(mockedGulp, ['', '', '']);
+      gulp.task('test', function (param) {
+        expect(param).to.be.undefined;
         done();
       });
     });
 
-    it('should pass "true" in the "param"-argument', function(done) {
-      var gulp =  gulpParam(mockedGulp, ['', '', '', '--param']);
-      gulp.task('test', function(param) {
-        assert.equal(param, true);
+    it('should pass "true" in the "param"-argument', function (done) {
+      var gulp = gulpParam(mockedGulp, ['', '', '', '--param']);
+      gulp.task('test', function (param) {
+        expect(param).to.be.true;
         done();
       });
     });
-        it('should pass the params in the correct order', function(done) {
-      var gulp =  gulpParam(mockedGulp,  ['', '', '', '--param1', 'value1', '--param2', 'value2']);
-      gulp.task('test', function(param1, param2) {
-        assert.equal(param1, 'value1');
-        assert.equal(param2, 'value2');
+    it('should pass the params in order defined by function', function (done) {
+      var gulp = gulpParam(mockedGulp, ['', '', '', '--param1', 'value1', '--param2', 'value2']);
+      gulp.task('test', function (param1, param2) {
+        expect(param1).to.be.equal('value1');
+        expect(param2).to.be.equal('value2');
+        done();
       });
+    });
+    it('should pass the params in the correct order even if they are define otherwise', function (done) {
+      var gulp = gulpParam(mockedGulp, ['', '', '', '--param2', 'value2', '--param1', 'value1']);
+      gulp.task('test', function (param2, param1) {
+        expect(param1).to.be.equal('value1');
+        expect(param2).to.be.equal('value2');
+        done();
+      });
+    });
+    it('should run callback as well', function (done) {
+      var gulp = gulpParam(mockedGulp, ['', '', '', '--param2', 'value2', '--param1', 'value1'],"callback");
+      gulp.task('test', function (callback, param2, param1) {
+        expect(param1).to.be.equal('value1');
+        expect(param2).to.be.equal('value2');
+        callback();
+      });
+    });
 
-     gulp =  gulpParam(mockedGulp,  ['', '', '', '--param2', 'value2', '--param1', 'value1']);
-      gulp.task('test', function(param2, param1) {
-        assert.equal(param1, 'value1');
-        assert.equal(param2, 'value2');
-        done();
-      });
-    });
   });
 });
