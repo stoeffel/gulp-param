a6a6cdc21244a102216aa0837439ff980f0c07c6
przemcio <przemyslaw.olech@gmail.com>
2016-02-02 23:46:29 +0100

    #20 clean up

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
-var retrieveArguments = require('retrieve-arguments')
-  , extend = require('lodash.assignin')
-  , minimist = require('minimist')
-  , stringify = require('node-stringify')
-  , debug = require('debug')('gulp-param');
+var retrieveArguments = require('retrieve-arguments'), minimist = require('minimist'),
+  _ =require('lodash');
 
-module.exports = function (gulp, processArgv, callbackFunctionName) {
+module.exports = function (gulp, processArgv, cf) {
   var parsedCmdArguments = minimist(processArgv);
-
-  var prepareArgumentsArray = function (functionArguments, originalCallbackFunction) {
-    var args = [];
-    debug('prepare argument base on function arguments (%s) and parsed commandline (%s)',
-      functionArguments, stringify(parsedCmdArguments));
-
-    for (var i = 0; i < functionArguments.length; i++) {
-      var functionArgument = functionArguments[i], value = parsedCmdArguments[functionArgument];
-      if (value) {
-        args.push(value);
-      } else if (functionArgument === (callbackFunctionName || "callback")) {
-        args.push(originalCallbackFunction);
-      } else {args.push(undefined);}
-    }
-    return args;
+  var pp =  (cf || "callback");
+  var prepareArgumentsArray = function (functionArguments, cmdArguments) {
+    return _.map(functionArguments,function(fa) {
+      return cmdArguments[fa];
+    });
   };
-
-  var origTask = gulp.task;
   var wrappedTask = function (taskName, taskDependencies, taskDefinition) {
     if (!taskDefinition && typeof taskDependencies === 'function') {
       taskDefinition = taskDependencies;
@@ -32,11 +17,11 @@ module.exports = function (gulp, processArgv, callbackFunctionName) {
     taskDefinition = taskDefinition || function () {};
 
     var wrappedTaskFunction = function (originalCallbackFunction) {
-      return taskDefinition.apply(gulp, prepareArgumentsArray(retrieveArguments(taskDefinition), originalCallbackFunction));
+      return taskDefinition.apply(gulp,
+        prepareArgumentsArray(retrieveArguments(taskDefinition), _.assign(
+          _.fromPairs([[pp,originalCallbackFunction]]),parsedCmdArguments)));
     };
-    return origTask.call(gulp, taskName, taskDependencies || [], wrappedTaskFunction);
+    return gulp.task.call(gulp, taskName, taskDependencies || [], wrappedTaskFunction);
   };
-
-
-  return extend({}, gulp, { task: wrappedTask });
+  return _.assignIn({}, gulp, { task: wrappedTask });
 };
